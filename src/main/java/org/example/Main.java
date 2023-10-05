e93d3de6b18810e7ed227ea9390f519bdc2fa737
Nikoss01 <nikodem.niklas@wp.pl>
2023-10-05 08:36:38 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(myMethod());
+        System.setProperty("webdriver.chrome.driver","E:\\Lekcyjne\\Słowik\\Projekt\\chromedriver.exe" );
+        ChromeOptions opt = new ChromeOptions();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+    }
+    private static String myMethod() {
+        return "my string";
+    }
+}
